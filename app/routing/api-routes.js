4112990771b0071d11aa67a3da9f92d1013e5bdd
CKK6 <you@example.com>
2016-07-31 23:48:45 -0400

    routes-dependencies

---
+++ app/routing/api-routes.js
@@ -0,0 +1,23 @@
+var friendArray = [];
+app.get('/api/friends', function(req, res){
+	res.sendFile(path.join(__dirname, 'survey.html'));
+})
+// We then display the JSON to the users
+ 	res.json(newFriend);
+ 
+
+
+// Take in data from survey- takes in JSON input
+ app.post('/api/friends', function(req, res){
+ 
+ 	// req.body hosts is equal to the JSON post sent from the user
+ 	var newFriend = req.body;
+ 
+ 	console.log(newFriend);
+ 
+ 	// We then add the json the user sent to the character array
+ 	characters.push(newFriend);
+ 
+ 	// We then display the JSON to the users
+ 	res.json(newFriend);
+ })
